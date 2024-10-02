bbb8245151d6bdcda557d8af2a6365353288ecd4
KaTicb <trick1204ed@mail.ru>
2024-10-02 19:26:47 +0300

    add move animation
    add some movement for character & collisions for ground

--- Characters/Bot/bot.gd
+++ Characters/Bot/bot.gd
@@ -1,9 +1,23 @@
 extends CharacterBody3D
 
+@export
+var movement_speed: float = 5
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export
+var jump_velocity: float = 6
 
+@export
+var mouse_sensitivity: float = 0.3
+
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate(Vector3.UP, deg_to_rad(-event.relative.x * mouse_sensitivity))
+		
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -12,17 +26,21 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("ui_right", "ui_left", "ui_down" , "ui_up")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * movement_speed
+		velocity.z = direction.z * movement_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, movement_speed)
+		velocity.z = move_toward(velocity.z, 0, movement_speed)
+		
+	$AnimationTree.set("parameters/conditions/walk", (velocity.x != 0 || velocity.z != 0) && is_on_floor())
+	$AnimationTree.set("parameters/conditions/tpose", velocity.x == 0 && velocity.z == 0 && is_on_floor())
+	$AnimationTree.set("parameters/conditions/tpose", !is_on_floor())
 
 	move_and_slide()
