c7ab0c15837efc0eba72d41a58e7c96e141d1163
KaTicb <trick1204ed@mail.ru>
2024-10-03 21:20:16 +0300

    add idle animation
    add idle animation

--- Characters/Bot/bot.gd
+++ Characters/Bot/bot.gd
@@ -18,19 +18,16 @@ func _input(event):
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
-	
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_velocity
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_right", "ui_left", "ui_down" , "ui_up")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
@@ -45,6 +42,8 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, movement_speed)
 		
 	$AnimationTree.set("parameters/conditions/walk", (velocity.x != 0 || velocity.z != 0) && is_on_floor())
-	$AnimationTree.set("parameters/conditions/tpose", (!velocity.x && !velocity.z && is_on_floor()) || !is_on_floor())
+	$AnimationTree.set("parameters/conditions/idle", !velocity.x && !velocity.z && is_on_floor())
+	$AnimationTree.set("parameters/conditions/tpose", !is_on_floor())
+	
 	
 	move_and_slide()
--- Characters/Bot/low_poly_robot.gd
+++ Characters/Bot/low_poly_robot.gd
@@ -4,9 +4,9 @@ extends Node3D
 func rotate_by_character(y, x):
 	if Input.is_action_pressed("ui_down"):
 		rotation.y = PI
-	if Input.is_action_pressed("ui_up"):
+	elif Input.is_action_pressed("ui_up"):
 		rotation.y = 0
-	if Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("ui_left"):
 		rotation.y = PI / 2
-	if Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed("ui_right"):
 		rotation.y = -PI / 2
