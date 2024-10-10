062f0b3389d30755abda59b75b350925a13f87ff
KaTicb <trick1204ed@mail.ru>
2024-10-10 17:31:04 +0300

    add wind trap
    add wind trap & fix some bot script for wind trap

--- Characters/Bot/bot.gd
+++ Characters/Bot/bot.gd
@@ -6,6 +6,9 @@ extends CharacterBody3D
 @export var fall_height: float = -4
 @export var hp: int = 100
 
+@export var is_deviate: bool = false
+var deviation: Vector3 = Vector3.ZERO
+
 func _input(event) -> void:
 	if event is InputEventMouseMotion:
 		rotate(Vector3.UP, deg_to_rad(-event.relative.x * mouse_sensitivity))
@@ -27,7 +30,7 @@ func _physics_process(delta: float) -> void:
 	
 	if hp <= 0:
 		game_over()
-	print(hp)
+	#print(hp)
 	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -44,14 +47,14 @@ func _physics_process(delta: float) -> void:
 	character_node.rotate_by_character()
 	
 	if direction:
-		velocity.x = direction.x * movement_speed
-		velocity.z = direction.z * movement_speed
+		velocity.x = direction.x * movement_speed + deviation.x
+		velocity.z = direction.z * movement_speed + deviation.z
 	else:
-		velocity.x = move_toward(velocity.x, 0, movement_speed)
-		velocity.z = move_toward(velocity.z, 0, movement_speed)
+		velocity.x = move_toward(velocity.x, 0, movement_speed) + deviation.x
+		velocity.z = move_toward(velocity.z, 0, movement_speed) + deviation.z
 		
-	$AnimationTree.set("parameters/conditions/walk", (velocity.x != 0 || velocity.z != 0) && is_on_floor())
-	$AnimationTree.set("parameters/conditions/idle", !velocity.x && !velocity.z && is_on_floor())
+	$AnimationTree.set("parameters/conditions/walk", (direction.x != 0 || direction.z != 0) && is_on_floor())
+	$AnimationTree.set("parameters/conditions/idle", !direction.x && !direction.z && is_on_floor())
 	$AnimationTree.set("parameters/conditions/tpose", !is_on_floor())
 
 	move_and_slide()
---
+++ Regular and trap blocks/wind.gd
@@ -0,0 +1,29 @@
+extends Area3D
+
+var is_body_inside: bool = false
+
+@export var wind_speed: float = 1
+
+func _on_body_entered(body: Node3D) -> void:
+	if body:
+		if body.has_method("take_damage"):
+			$WindParticles.emitting = true
+			is_body_inside = true
+			print("en")
+			body.is_deviate = true
+			while is_body_inside:
+				rotation.y = deg_to_rad(randi_range(0, 360))
+				
+				body.deviation = transform.basis.x.normalized() * wind_speed
+				print(body.velocity)
+				await get_tree().create_timer(2).timeout
+				
+
+
+func _on_body_exited(body: Node3D) -> void:
+	if body:
+		if body.has_method("take_damage"):
+			$WindParticles.emitting = false
+			body.deviation = Vector3.ZERO
+			is_body_inside = false
+			print("ex")
