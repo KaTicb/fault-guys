5a51cbea73d991f4a086da9e8a22e67d118d0843
KaTicb <trick1204ed@mail.ru>
2024-10-12 16:19:10 +0300

    Clear code fix
    Clear code fix

--- finish.gd
+++ finish.gd
@@ -5,5 +5,5 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body:
 		if body.has_method("take_damage"):
 			var time_elapsed = $"../TimeLabel".time_elapsed
-			EventBus.emit_signal("finish_progress", time_elapsed)
+			EventBus.finish_progress.emit(time_elapsed)
 			get_tree().change_scene_to_file("res://finish_panel.tscn")
