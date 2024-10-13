521c2dfbd7f1ebd9bafc55e698c46744aa23766c
KaTicb <trick1204ed@mail.ru>
2024-10-13 12:30:34 +0300

    add new rotation sides
    add new rotation sides for character

--- Characters/Bot/low_poly_robot.gd
+++ Characters/Bot/low_poly_robot.gd
@@ -3,9 +3,21 @@ extends Node3D
 
 func rotate_by_character():
 	if Input.is_action_pressed("ui_down"):
-		rotation.y = PI
+		if Input.is_action_pressed("ui_left"):
+			rotation.y = 3 * PI / 4
+		elif Input.is_action_pressed("ui_right"):
+			rotation.y = - 3 * PI / 4
+		else:
+			rotation.y = PI
+			
 	elif Input.is_action_pressed("ui_up"):
-		rotation.y = 0
+		if Input.is_action_pressed("ui_left"):
+			rotation.y = PI / 4
+		elif Input.is_action_pressed("ui_right"):
+			rotation.y = - PI / 4
+		else:
+			rotation.y = 0
+		
 	elif Input.is_action_pressed("ui_left"):
 		rotation.y = PI / 2
 	elif Input.is_action_pressed("ui_right"):
